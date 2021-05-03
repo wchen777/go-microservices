d3a8578c395c2b246d7dda344aaa7fe8988315a8
wchen77 <william_b_chen@brown.edu>
2021-05-03 12:25:59 -0400

    episode 1

--- main.go
+++ main.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
+	"net/http"
 )
 
+// func testPath() {
+// 	gopath := os.Getenv("GOPATH")
+// 	if gopath == "" {
+// 		log.Fatal("Your GOPATH has not been set!")
+// 	}
+
+// 	path := os.Getenv("PATH")
+// 	gobin := filepath.Join(gopath, "bin")
+// 	if !strings.Contains(path, gobin) {
+// 		log.Fatalf("Your PATH does not contain %s", gobin)
+// 	}
+
+// 	log.Println("Success!")
+// }
+
 func main() {
-	
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		log.Fatal("Your GOPATH has not been set!")
-	}
 
-	path := os.Getenv("PATH")
-	gobin := filepath.Join(gopath, "bin")
-	if !strings.Contains(path, gobin) {
-		log.Fatalf("Your PATH does not contain %s", gobin)
-	}
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("hello")
+		d, err := ioutil.ReadAll((r.Body))
 
-	log.Println("Success!")
+		// err check w/ response code
+		if err != nil {
+			http.Error(rw, "error caught", http.StatusBadRequest)
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("error caught"))
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("goodbye")
+	})
+
+	// start server
+	http.ListenAndServe(":9090", nil)
+
 }
